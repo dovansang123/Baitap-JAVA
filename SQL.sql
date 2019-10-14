edd1bad72b8305c1d7fe39995db8c61a06d830c1
luuvanthanh <53346824+luuvanthanh@users.noreply.github.com>
2019-10-14 21:08:50 +0700

    Do van sang

--- SQL.sql
+++ SQL.sql
@@ -8,34 +8,39 @@ create table Student
     name nvarchar(100) ,
 	age int ,
 )
-SELECT name age
-FROM Student
-INSERT INTO Student
- VALUES('Nguyen Van A','18')
- INSERT INTO Student
- VALUES('Nguyen Van B','18')
+create table Class
+(
+	id int IDENTITY(1,1)  NOT NULL primary key,
+	class_name nvarchar(100),
+)
+create table DayOff
+(
+	id int IDENTITY(1,1)  NOT NULL primary key,
+    student_id varchar(11) ,
+	day_off date,
+)
+
+
+create database QuanLySinhVien
+go 
+use QuanLySinhVien
+go
+create table Student
+(
+	id int IDENTITY(1,1)  NOT NULL primary key,
+    name nvarchar(100) ,
+	age int ,
+)
 create table Class
 (
 	id int IDENTITY(1,1)  NOT NULL,
 	class_name nvarchar(100),
+	primary key (id) foreign key (id) references Student(id),
 )
-SELECT class_name
-FROM Class
-INSERT INTO Class
- VALUES('java 101')
- INSERT INTO Class
- VALUES('co so du lieu')
- INSERT INTO Class
- VALUES('toan cao cap')
 create table DayOff
 (
 	id int IDENTITY(1,1)  NOT NULL ,
     student_id varchar(11) ,
 	day_off date,
+	primary key (id) foreign key (id) references Class(id),
 )
-SELECT student_id,day_off
-FROM DayOff
-INSERT INTO DayOff
- VALUES('01234567890','2/9/2019')
- INSERT INTO DayOff
- VALUES('01234567891','3/9/2019')
